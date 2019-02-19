2623a22530a36db0a4f0a2fe1db98ae69fd2c3ae
abitravers1989 <atravers4007@gmail.com>
2019-02-19 11:50:44 +0000

    start JavaScript solution

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-const csvParse = require('csv-parse');
+const csv= require('csv-parse');
 const fs = require('fs');
 const results = [];
 
-const testData = fs.createReadStream('./data/train.csv')
-  .pipe(csvParse())
-  .on('data', (data) => results.push(data))
-  .on('end', () => {
-    console.log('----->', results)
-});
+// const parser = csvParse({
+//   separator: ','
+// }, (err, data) => {
+//   if(err) console.log('----->', err)
+//   setTimeout(() => {console.log('----->', data)}, 5000)
+// })
+
+
+fs.createReadStream('./data/train.csv').pipe(parser)
+//   .on('data', (data) => results.push(data))
+//   .on('end', () => {
+//     console.log('----->', results)
+// });
+
+// fs.createReadStream('./data/train.csv')
+//   .pipe(csv({ delimiter: ',', auto_parse: true}))
+//   .on('data', results.push)
+//   .on('end', () => {
+//     console.log(results);
+//     // [
+//     //   { NAME: 'Daffy Duck', AGE: 24 },
+//     //   { NAME: 'Bugs Bunny', AGE: 22 }
+//     // ]
+//   })
+//   .on('error', (err) => {console.log('----->', err)})
+
+  
